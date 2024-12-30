77063f5b9044e1bb25b72e602e109e016eaa692d
Samira Rodrigues Silva <srs.samira.silva@gmail.com>
2024-12-30 10:54:24 -0300

    add address mapper to clean code

---
+++ address-api/src/main/java/br/com/zup/address/services/mappers/AddressMapper.java
@@ -0,0 +1,30 @@
+package br.com.zup.address.services.mappers;
+
+import br.com.zup.address.controllers.dtos.AddressRequestDTO;
+import br.com.zup.address.controllers.dtos.AddressResponseDTO;
+import br.com.zup.address.models.Address;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressMapper {
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    public static Address toAddress(AddressRequestDTO addressRequestDTO) {
+        return mapper.convertValue(addressRequestDTO, Address.class);
+    }
+
+    public static AddressResponseDTO toAddressResponseDTO(Address address) {
+        return mapper.convertValue(address, AddressResponseDTO.class);
+    }
+
+    public static List<AddressResponseDTO> toAddressResponseDTOList(List<Address> addresses) {
+        List<AddressResponseDTO> addressResponseDTOs = new ArrayList<>();
+        for (Address address : addresses) {
+            addressResponseDTOs.add(toAddressResponseDTO(address));
+        }
+        return addressResponseDTOs;
+    }
+}
