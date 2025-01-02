8b1c79308b53b468cd4bce96e9d773c393051377
Samira Rodrigues Silva <srs.samira.silva@gmail.com>
2025-01-02 11:11:40 -0300

    add logs in ConsumerAddressController

--- gateway/src/main/java/br/com/zup/gateway/controllers/ConsumerAddressController.java
+++ gateway/src/main/java/br/com/zup/gateway/controllers/ConsumerAddressController.java
@@ -8,9 +8,11 @@ import br.com.zup.gateway.infra.clients.consumer.dtos.ConsumerRegisterDTO;
 import br.com.zup.gateway.infra.clients.consumer.dtos.ConsumerResponseDTO;
 import br.com.zup.gateway.services.ConsumerAddressService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/consumer-address")
@@ -19,48 +21,61 @@ public class ConsumerAddressController {
     @Autowired
     private ConsumerAddressService consumerAddressService;
 
+    private static final Logger LOG = Logger.getLogger(ConsumerAddressController.class.getName());
+
     @PostMapping
-    public ConsumerAddressResponseDTO register(@RequestBody ConsumerAddressRegisterDTO registerDTO){
-        return consumerAddressService.registerConsumerAddress(registerDTO);
+    public ResponseEntity<ConsumerAddressResponseDTO> register(@RequestBody ConsumerAddressRegisterDTO registerDTO){
+        LOG.info("Register consumer address");
+        return ResponseEntity.status(201).body(consumerAddressService.registerConsumerAddress(registerDTO));
     }
 
     @GetMapping("/address/{addressId}")
-    public AddressResponseDTO findById(@PathVariable("addressId") String addressId){
-        return consumerAddressService.getAddressById(addressId);
+    public ResponseEntity<AddressResponseDTO> findById(@PathVariable("addressId") String addressId){
+        LOG.info("Find consumer address by ID");
+        return ResponseEntity.ok(consumerAddressService.getAddressById(addressId));
     }
 
     @GetMapping("/address")
-    public List<AddressResponseDTO> findAllAddresses(){
-        return consumerAddressService.getAllAddresses();
+    public ResponseEntity<List<AddressResponseDTO>> findAllAddresses(){
+        LOG.info("Find all consumer addresses");
+        return ResponseEntity.ok(consumerAddressService.getAllAddresses());
     }
 
     @GetMapping("/consumer")
-    public List<ConsumerResponseDTO> findAllConsumerAddresses(){
-        return consumerAddressService.getAllConsumers();
+    public ResponseEntity<List<ConsumerResponseDTO>> findAllConsumerAddresses(){
+        LOG.info("Find all consumer addresses");
+        return ResponseEntity.ok(consumerAddressService.getAllConsumers());
     }
 
     @GetMapping("/consumer/{consumerId}")
-    public ConsumerResponseDTO findByConsumerId(@PathVariable("consumerId") String consumerId){
-        return consumerAddressService.getConsumerById(consumerId);
+    public ResponseEntity<ConsumerResponseDTO> findByConsumerId(@PathVariable("consumerId") String consumerId){
+        LOG.info("Find consumer by ID");
+        return ResponseEntity.ok(consumerAddressService.getConsumerById(consumerId));
     }
 
     @PutMapping("/address/{addressId}")
-    public AddressResponseDTO updateAddress(@PathVariable("addressId") String addressId, @RequestBody AddressRegisterDTO addressRegisterDTO){
-        return consumerAddressService.updateAddressById(addressId, addressRegisterDTO);
+    public ResponseEntity<AddressResponseDTO> updateAddress(@PathVariable("addressId") String addressId, @RequestBody AddressRegisterDTO addressRegisterDTO){
+        LOG.info("Update consumer address");
+        return ResponseEntity.ok(consumerAddressService.updateAddressById(addressId, addressRegisterDTO));
     }
 
     @PutMapping("/consumer/{consumerId}")
-    public ConsumerResponseDTO updateConsumer(@PathVariable("consumerId") String consumerId, @RequestBody ConsumerRegisterDTO registerDTO){
-        return consumerAddressService.updateConsumer(consumerId, registerDTO);
+    public ResponseEntity<ConsumerResponseDTO> updateConsumer(@PathVariable("consumerId") String consumerId, @RequestBody ConsumerRegisterDTO registerDTO){
+        LOG.info("Update consumer address");
+        return ResponseEntity.ok(consumerAddressService.updateConsumer(consumerId, registerDTO));
     }
 
     @DeleteMapping("{address/addressId}")
-    public void deleteAddress(@PathVariable("addressId") String addressId){
+    public ResponseEntity<?> deleteAddress(@PathVariable("addressId") String addressId){
+        LOG.info("Delete consumer address by ID");
         consumerAddressService.deleteAddressById(addressId);
+        return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("consumer/{consumerId}")
-    public void deleteConsumer(@PathVariable("consumerId") String consumerId){
+    public ResponseEntity<?> deleteConsumer(@PathVariable("consumerId") String consumerId){
+        LOG.info("Delete consumer by ID");
         consumerAddressService.deleteConsumerById(consumerId);
+        return ResponseEntity.noContent().build();
     }
 }
